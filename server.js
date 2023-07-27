5f7389e1623eea51ac288c2d1caa5f984a91c8d1
onetimefixation <onetimefixation@gmail.com>
2023-07-27 16:56:58 -0400

    starting point

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require("path");
 
 const app = express();
 
+// Serve static files from the "assets" directory in the root folder
+app.use(express.static(path.join(__dirname, "assets")));
+
+// Serve static files from the "images" directory in the root folder
+app.use(express.static(path.join(__dirname, "images")));
+
 // Serve the index.html file as the home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
