422cda6157ca96419d2994187816f76aba1b5bc7
onetimefixation <onetimefixation@gmail.com>
2023-08-06 16:08:30 -0400

    base for writing to mongo

--- server.js
+++ server.js
@@ -1,34 +1,142 @@
-const express = require("express");
-const path = require("path");
-const serveStatic = require("serve-static");
-
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-// Set the MIME type for CSS files
-app.use('/assets/css', express.static(__dirname + '/assets/css', { type: 'text/css' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+const port = 3000;
 
-/* 
-// Serve static files from the "assets" directory in the root folder
-app.use(express.static(path.join(__dirname, "/assets/css")));
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = 'mongodb+srv://MrBeckHam187:squ1rrelsClimbandH1dePecans)@musiqvp.q02lkpj.mongodb.net/?retryWrites=true&w=majority';
 
-// Serve static files from the "images" directory in the root folder
-app.use(express.static(path.join(__dirname, "/images")));
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+// GET endpoint to read data from "users" collection
+app.get('/users', async (req, res) => {
+    try {
+      // Connect to the database
+      await client.connect();
+      const database = client.db('musiqvp-db'); 
+      const collection = database.collection('users'); 
+
+ // ***************** Logic to retrieve display the data goes here *************// 
+      // Query the database to get all documents from "users" collection
+      const users = await collection.find({}).toArray();
+      res.json(users); // Send the data as a JSON response
+
+console.log("Here is the data")
+console.log (users)
+// ******************************************************************//
+
+    } catch (error) {
+      console.error('Error reading data from MongoDB:', error);
+      res.status(500).json({ success: false, message: 'Error reading data from MongoDB' });
+    } finally {
+      // Close the client connection
+      await client.close();
+    }
+  });
+
+  // GET endpoint to read data from "users" collection
+app.post('/adduser', async (req, res) => {
+    try {
+      // Connect to the database
+      await client.connect();
+      const database = client.db('musiqvp-db'); 
+      const collection = database.collection('users'); 
+
+ // ***************** Logic to add a user record goes here *************// 
+    
+ const { firstname, lastname, country, age, email, location, additionalinfo } = req.body;
+
+ // destructure the req.body //
+ const registrationData = {
+   firstname,
+   lastname,
+   country,
+   age,
+   email,
+   location,
+   additionalinfo,
+   status: 'pending'
+ };
+  
+      
+      collection.insertOne(registrationData, (err, result) => {
+
+        client.close();
+  
+        if (err) {
+          console.error('Error inserting data into MongoDB:', err);
+          res.status(500).json({ success: false });
+          return;
+        }
+
+      });
+ 
+ // For testing purposes, write the data back out //
+
+      const users = await collection.find({}).toArray();
+      res.json(users); // Send the data as a JSON response
+      /*         res.json({ success: true });
+        res.send('Data received successfully!');  */
+
+  // Send an HTML response with the success message
+
+/* res.send(`
+  <html>
+    <head>
+      <title>Success</title>
+    </head>
+    <body>
+      <h1>Data received successfully!</h1>
+    </body>
+  </html>
+`);
  */
 
-// Serve static files from the "assets" directory in the root folder
-app.use(serveStatic(path.join(__dirname, "assets")));
+console.log("Here is the data")
+console.log (users)
+// ******************************************************************//
 
-// Serve static files from the "images" directory in the root folder
-app.use(serveStatic(path.join(__dirname, "images")));
+    } catch (error) {
+      console.error('Error reading data from MongoDB:', error);
+      res.status(500).json({ success: false, message: 'Error reading data from MongoDB' });
+    } finally {
+      // Close the client connection
+      await client.close();
+    }
+  });
 
 
-// Serve the index.html file as the home page
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+// Start the server
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+    console.log(`Server listening on port ${PORT}`);
+  });
+
+// Connect to the database
+
+run().catch(console.dir);
